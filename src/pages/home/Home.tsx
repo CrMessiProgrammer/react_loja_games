1fa816bd347ee706270a3ff21388c0c9017ad1d4
Carlos Henrique Nunes <carloshnunes383@gmail.com>
2025-02-19 18:51:10 -0300

    :sparkles: Componentes Navbar e Footer

--- src/pages/home/Home.tsx
+++ src/pages/home/Home.tsx
@@ -1,18 +1,31 @@
+
 function Home() {
     return (
         <>
-            <div className="w-screen flex justify-center">
-                <div>
-                    <div className="max-w-7xl flex flex-col items-center">
-                        <h2>Seja Bem Vindo(a) Gamer!</h2>
-                        <p>Vamos jogar?🎮</p>
+            <div className="bg-indigo-900 flex justify-center">
+                <div className='container grid grid-cols-2 text-white'>
+                    <div className="flex flex-col gap-4 items-center justify-center py-4">
+                        <h2 className='text-5xl font-bold'>
+                            Seja Bem Vindo(a) Gamer!
+                        </h2>
+                        <p className='text-xl'>
+                            Vamos jogar?🎮
+                        </p>
+
+                        <div className="flex justify-around gap-4">
+                            <div className='rounded text-white 
+                                            border-white border-solid border-2 py-2 px-4'
+                                >
+                                Novo Produto
+                            </div>
+                        </div>
                     </div>
 
-                    <div className="max-w-7xl flex flex-col items-center">
+                    <div className="flex justify-center ">
                         <img
                             src="https://ik.imagekit.io/vzr6ryejm/games/home.png?updatedAt=1714775484705"
-                            alt="Imagem da Página Home"
-                            width="400px"
+                            alt="Imagem Página Home"
+                            className='w-2/3'
                         />
                     </div>
                 </div>
